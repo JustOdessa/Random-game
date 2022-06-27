61001295df3a66694255688f2b9e287245964cd4
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-06-27 12:08:16 +0300

    third commit

--- main.py
+++ main.py
@@ -1,65 +1,63 @@
 import random
 
-print('Hello World!')
-
 
 def game():
-    Correct = 0
-    Wrong = 0
+    correct = 0
+    wrong = 0
 
-    Numbers = input(
+    numbers = input(
         'Type two numbers divided by a backspace, in a range of which you would like to guess, or type quit '
         'if you want to exit: ')
-    Numbers_range = Numbers.split(' ')
+    numbers_range = numbers.split(' ')
 
     try:
-        int(Numbers_range[0])
+        int(numbers_range[0])
     except ValueError:
         print('Oops, probably you have written something apart from numbers')
-        print(f'{Correct}:{Wrong}')
-        if Correct > Wrong:
-            print('You won!')
-        elif Correct == Wrong:
-            print('It`s a draw')
-        else:
-            print('You lost!')
         game()
 
     try:
         while True:
 
-            Player_choice = input('Guess the number: ')
+            player_choice = input('Guess the number: ')
 
-            if Player_choice != 'quit':
+            if player_choice != 'quit':
 
-                Random_number = random.randint(int(Numbers_range[0]), int(Numbers_range[1]))
+                random_number = random.randint(int(numbers_range[0]), int(numbers_range[1]))
 
-                Player_choice_int = int(Player_choice)
+                player_choice_int = int(player_choice)
 
-                if Player_choice_int in range(int(Numbers_range[0]), int(Numbers_range[1]) + 1):
-                    print(Player_choice_int)
-                    print(Random_number)
+                if player_choice_int in range(int(numbers_range[0]), int(numbers_range[1]) + 1):
+                    print(player_choice_int)
+                    print(random_number)
 
-                    if Player_choice_int == Random_number:
-                        Correct += 1
+                    if player_choice_int == random_number:
+                        correct += 1
                         print('You guessed!')
                     else:
-                        Wrong += 1
+                        wrong += 1
                         print("You didn't guess")
                 else:
-                    print(f'Your number must be between {int(Numbers_range[0])} and {int(Numbers_range[1])}')
+                    print(f'Your number must be between {int(numbers_range[0])} and {int(numbers_range[1])}')
 
             else:
-                print(f'{Correct}:{Wrong}')
-                if Correct > Wrong:
+                print(f'{correct}:{wrong}')
+                if correct > wrong:
                     print('You won!')
-                elif Correct == Wrong:
+                elif correct == wrong:
                     print('It`s a draw')
                 else:
                     print('You lost!')
                 exit()
     except ValueError:
         print('Oops, probably you have written something apart from numbers')
+        print(f'{correct}:{wrong}')
+        if correct > wrong:
+            print('You won!')
+        elif correct == wrong:
+            print('It`s a draw')
+        else:
+            print('You lost!')
         game()
 
 
